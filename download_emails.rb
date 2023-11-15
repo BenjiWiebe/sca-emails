0ae56a7d728be958a5281611c5270f96a2fc8561
Benji Wiebe <benjiwiebe14@gmail.com>
2023-11-15 15:05:08 -0600

    Add progress indication to email downloading

--- download_emails.rb
+++ download_emails.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/ruby
 require 'net/pop'
+require 'pry'
 
 require_relative 'email_secrets.rb'
 pop = Net::POP3.new(EmailSecrets::HOSTNAME)
@@ -9,13 +10,15 @@ pop.start(EmailSecrets::ACCOUNT, EmailSecrets::PASSWORD)
 if pop.mails.empty?
 	puts 'No mail.'
 else
-	puts 'Downloading new emails...'
+	puts "Downloading #{pop.mails.length} new emails..."
 	pop.each_mail do |m|
 		File.open("inbox/#{m.uidl}", 'w') do |f|
 			f.write m.pop
 			m.delete
 		end
+		print '.'
 	end
+	puts
 	puts "#{pop.mails.size} emails saved."
 end
 pop.finish
